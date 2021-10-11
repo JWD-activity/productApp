24a2aeccff20073548007718564b05315b684827
Jinok <joseo46@hotmail.com>
2021-10-11 18:34:40 +1100

    fix search bar and filter position

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -13,29 +13,34 @@ function Navbar() {
       style={{ position: 'fixed', zIndex: 1, width: '100%' }}
     >
       <nav className='menu'>
-        <Row justify='space-between'>
-          <Col className='logo' xs={20} sm={4}>
+        <Row align='middle'>
+          <Col className='logo' sm={2}>
             <a href='/'>Products</a>
           </Col>
-          <Col className='search' xs={2} sm={10}>
-            <Search
-              placeholder='Search'
-              // onSearch={onSearch}
-              enterButton
-            />
-          </Col>
-          <Col xs={2} sm={10}>
-            <Select
-              showSearch
-              style={{ width: 200 }}
-              placeholder='Sort by'
-              aria-label='sorting select option'
-            >
-              <Option value='1'>Apphbeically, A-Z</Option>
-              <Option value='2'>Apphbeically, Z-A</Option>
-              <Option value='3'>Row Price</Option>
-              <Option value='4'>High Price</Option>
-            </Select>
+          <Col sm={22}>
+            <Row className='menu__input'>
+              <Col className='search' sm={14}>
+                <Search
+                  aria-label='Search for products '
+                  placeholder='Search'
+                  // onSearch={onSearch}
+                  enterButton
+                />
+              </Col>
+              <Col className='filter' sm={10}>
+                <Select
+                  showSearch
+                  style={{ width: 200 }}
+                  placeholder='Sort by'
+                  aria-label='sorting select option'
+                >
+                  <Option value='1'>Apphbeically, A-Z</Option>
+                  <Option value='2'>Apphbeically, Z-A</Option>
+                  <Option value='3'>Row Price</Option>
+                  <Option value='4'>High Price</Option>
+                </Select>
+              </Col>
+            </Row>
           </Col>
         </Row>
       </nav>
