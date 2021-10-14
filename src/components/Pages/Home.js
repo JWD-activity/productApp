47e5cf4aa706a9c7cf358afcd51bd7388f9376ad
Jinok <joseo46@hotmail.com>
2021-10-14 14:20:55 +1100

    add search

--- src/components/Pages/Home.js
+++ src/components/Pages/Home.js
@@ -2,6 +2,9 @@ import React, { useContext, useState } from 'react';
 import { ProductsContext } from '../../contexts/ProductsContext';
 import { SearchTermContext } from '../../contexts/SearchTermContext';
 import ProductCard from '../ProductCard/ProductCard';
+import Typography from '@mui/material/Typography';
+import Alert from '@mui/material/Alert';
+import AlertTitle from '@mui/material/AlertTitle';
 import Container from '@mui/material/Container';
 import Grid from '@mui/material/Grid';
 import CircularProgress from '@mui/material/CircularProgress';
@@ -16,42 +19,49 @@ function ProductList() {
 
   const searchProduct = (userInput) => {
     return post.filter((product) => {
-      if (userInput) {
-        return searchParam.some((newProduct) => {
-          return (
-            product[newProduct]
-              .toString()
-              .toLowerCase()
-              .indexOf(userInput.toLowerCase()) > -1
-          );
-        });
-        // .toLowerCase()
-        // .includes(userInput.toLowerCase());
-      }
-      return product;
+      return searchParam.some((newProduct) => {
+        return (
+          product[newProduct]
+            .toString()
+            .toLowerCase()
+            .indexOf(userInput.toLowerCase()) > -1
+        );
+      });
     });
+  };
+
+  const displayProducts = () => {
+    const results = searchProduct(search);
+    if (results.length > 0) {
+      return results.map((product) => (
+        <ProductCard
+          key={product._id}
+          id={product._id}
+          imgUrl={product.imgURL}
+          name={product.name}
+          price={product.price}
+          loading={loading}
+        />
+      ));
+    } else
+      return (
+        <Alert severity='info'>
+          <AlertTitle>Search results</AlertTitle>
+          Sorry We couldn't find any results— <strong>No results found.</strong>
+        </Alert>
+      );
   };
 
   return (
     <Container component='main'>
-      {loading ? (
-        <Grid container sx={{ justifyContent: 'center' }}>
-          <CircularProgress color='secondary' />
-        </Grid>
-      ) : (
-        <Grid container spacing={{ xs: 6, sm: 8, lg: 10 }} columns={12}>
-          {searchProduct(search).map((product) => (
-            <ProductCard
-              key={product._id}
-              id={product._id}
-              imgUrl={product.imgURL}
-              name={product.name}
-              price={product.price}
-              loading={loading}
-            />
-          ))}
-        </Grid>
-      )}
+      <Grid
+        container
+        spacing={{ xs: 6, sm: 8, lg: 10 }}
+        columns={12}
+        sx={{ justifyContent: 'center' }}
+      >
+        {loading ? <CircularProgress color='secondary' /> : displayProducts()}
+      </Grid>
     </Container>
   );
 }
